3e800f4d76e20462795a81a52a43eb5ca95da97d
Stanislav Gorovoy <cnfc19932@gmail.com>
2018-04-06 20:46:48 +0300

    modified

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Main from './components/main';
-import ScrollToTop from 'react-scroll-up';
-
-
 
 class App extends React.Component {
     render () { 
-        let s = {
-            fontSize: '38px',
-            color: '#000'
-        };
-    return (<div>
-        <ScrollToTop showUnder={160}>
-        <i class="fa fa-arrow-circle-o-up fa-3" style={s} aria-hidden="true"></i>
-  </ScrollToTop>
-        <Main/></div>); 
+        return (<div>Start</div>); 
     };
 } 
-/*
-var a = document.createElement('div');
-a.setAttribute("id","app");
-console.log('test');
-document.getElementById('etsp-ajax-container').appendChild(a);
-*/
 
 render(<App/>, document.getElementById('app'));  
